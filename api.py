13288d6b646337ea5d13b5f76e4c9cc5b86ac356
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2021-10-18 12:20:53 -0300

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-cors = CORS(app, resouce={r'/*':{'origins': '*'}})
+cors = CORS()
 
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite'
 

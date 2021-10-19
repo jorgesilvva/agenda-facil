e5319ad02a339d11d0c85e030071ede5ae51f1cb
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2021-10-19 10:40:41 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+ def __init__(self, cliente, datahora, email, contato, servico):
+        self.cliente = cliente
+        self.datahora = datahora
+        self.email = email
+        self.contato = contato
+        self.servico = servico

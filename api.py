33ba1a114e656287b42448f01f188e4c893d5649
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2021-10-19 07:17:35 -0300

    Update api.py

--- api.py
+++ api.py
@@ -96,4 +96,4 @@ def editar(id):
     return render_template('editar.html', cadastrado=cadastrado)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)

6870ef60f6e18bf576c90b66bb85e01f7a94258d
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2021-10-19 10:44:19 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
- def __init__(self, cliente, datahora, email, contato, servico):
-        self.cliente = cliente
-        self.datahora = datahora
-        self.email = email
-        self.contato = contato
-        self.servico = servico
